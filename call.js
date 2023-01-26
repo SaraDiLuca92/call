f733fc92c9631045fa7166de665938d04a6a37d7
Sara Di Luca <119141903+SaraDiLuca92@users.noreply.github.com>
2023-01-26 17:13:47 +0100

    Add files via upload

--- call.js
+++ call.js
@@ -3,12 +3,11 @@ fetch("https://striveschool-api.herokuapp.com/books")
     return response.json();
   })
   .then(function (books) {
-    let placeholder = document.querySelector(".primo");
+    let placeholder = document.querySelector(".row");
     let out = "";
     for (let book of books) {
-      out += ` <<div class="row row-cols-1 row-cols-md-3 g-4">
-      <div class="col">
-        <div class="card h-100">
+      out += ` <div class="col">
+        <div class="card ">
           <img src="${book.img} " class="card-img-top" alt="$book.img">
           <div class="card-body">
             <h5 class="card-title">${book.title}</h5>
@@ -19,32 +18,7 @@ fetch("https://striveschool-api.herokuapp.com/books")
           </div>
         </div>
       </div>
-      <div class="col">
-        <div class="card h-100">
-          <img src="${book.img}" class="card-img-top" alt="$book.img">
-          <div class="card-body">
-            <h5 class="card-title">${book.title}</h5>
-            <p class="card-text">price:${book.price} asin:${book.asin} category:${book.category}</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <img src="${book.img}" class="card-img-top" alt="${book.img}">
-          <div class="card-body">
-            <h5 class="card-title">${book.title}</h5>
-            <p class="card-text">price:${book.price} asin:${book.asin} category:${book.category}</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </div>
-        </div>
-      </div>
-    </div>
-       `;
+    `;
     }
     placeholder.innerHTML = out;
   });
